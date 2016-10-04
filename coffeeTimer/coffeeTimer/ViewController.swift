1eeea524be865475a2ee76777c7ebf6d95e48b17
Noah Berman <noah@noahberman.org>
2016-10-03 23:48:38 -0600

    initial setup and commit

---
+++ coffeeTimer/coffeeTimer/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  coffeeTimer
+//
+//  Created by Noah Berman on 10/3/16.
+//  Copyright © 2016 Faceless Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackgroundImage()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setBackgroundImage() {
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "background.jpg")
+        self.view.insertSubview(backgroundImage, at: 0)
+    }
+    
+
+}
